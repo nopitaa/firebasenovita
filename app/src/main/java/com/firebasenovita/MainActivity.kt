f725a8b69e37441acb15a7ac016067bf695b1eed
nopitaa <novitasyahwahapsari@gmail.com>
2022-03-18 14:25:37 +0700

    Initial commit

---
+++ app/src/main/java/com/firebasenovita/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.firebasenovita
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var ref : DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ref = FirebaseDatabase.getInstance().getReference("USERS")
+
+        btnSave.setOnClickListener {
+            savedata()
+            val intent = Intent (this, Show::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun savedata() {
+        val nama = inputNama.text.toString()
+        val status = inputStatus.text.toString()
+
+        val user = Users(nama,status)
+        val userId = ref.push().key.toString()
+
+        ref.child(userId).setValue(user).addOnCompleteListener {
+            Toast.makeText(this, "Successs",Toast.LENGTH_SHORT).show()
+            inputNama.setText("")
+            inputStatus.setText("")
+        }
+    }
+}
---
+++ app/src/main/java/com/firebasenovita/Users.kt
@@ -0,0 +1,8 @@
+package com.firebasenovita
+
+class Users (var nama : String, var status: String) {
+
+    constructor() : this("", "") {
+
+    }
+}
